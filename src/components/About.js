71d7ab31ced4e79c04f8a4a34ba90d041ede3c42
devsahadat <cse.devsahadat@gmail.com>
2023-03-02 16:34:04 +0600

    About Section Done

--- src/components/About.js
+++ src/components/About.js
@@ -17,9 +17,21 @@ const About = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
           {/* text */}
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
             <h2 className="h2 text-accent ">About me.</h2>
             <h3 className="h3 mb-4">
               I am a web developer with years of experience.
@@ -74,7 +86,7 @@ const About = () => {
                 My Portfolio
               </a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
